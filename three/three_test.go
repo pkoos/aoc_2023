e596bf2d6feb944ccfc56a6e589a9d209509ebcf
Paul Koos <paully_k5298@yahoo.com>
2023-12-15 14:46:00 -0800

    refactor tests to be more go-like.

--- three/three_test.go
+++ three/three_test.go
@@ -22,20 +22,22 @@ func TestFindSpecificPart(t *testing.T) {
 	data, _ := utils.String_slice_file(TEST_INPUT_FILE)
 	parts := find_part_numbers(data)
 	adjacent := find_adjacent_digits(data, find_symbols(data, SYMBOLS))
-	test_data, err := os.ReadFile("test_files/test_found_parts")
-	if err != nil {
+	
+	var test_data []byte
+	var err error
+	if test_data, err = os.ReadFile("test_files/test_found_parts");err != nil {
 		t.Fatalf("Error: %s\n", err)
 	}
+	
 	var expecteds []part_number
-	err = json.Unmarshal(test_data, &expecteds)
-	if err != nil {
+	if err = json.Unmarshal(test_data, &expecteds);err != nil {
 		t.Fatalf("Error: %s\n", err)
 	}
 
 	for idx, adj := range adjacent {
-		actual := find_specific_partnum(adj, parts)
 		expected := expecteds[idx]
-		if actual != expected {
+		
+		if actual := find_specific_partnum(adj, parts); actual != expected {
 			t.Errorf("%d: expected: %+v, actual: %+v\n", idx, expected, actual)
 		}
 	}
@@ -45,18 +47,19 @@ func TestPartsUnique(t *testing.T) {
 	data, _ := utils.String_slice_file(TEST_INPUT_FILE)
 	adjacent := find_adjacent_digits(data, find_symbols(data, SYMBOLS))
 	actual_unique_parts := parts_unique(adjacent, find_part_numbers(data))
-	test_data, err := os.ReadFile("test_files/test_unique_parts")
-	if err != nil {
+	
+	var test_data []byte
+	var err error
+	if test_data, err = os.ReadFile("test_files/test_unique_parts"); err != nil {
 		t.Fatalf("Error: %s\n", err)
 	}
+	
 	var expected_unique_parts []part_number
-	err = json.Unmarshal(test_data, &expected_unique_parts)
-	if err != nil {
+	if err = json.Unmarshal(test_data, &expected_unique_parts);err != nil {
 		t.Fatalf("Error: %s\n", err)
 	}
 	for idx, actual := range actual_unique_parts {
-		expected := expected_unique_parts[idx]
-		if actual != expected {
+		if expected := expected_unique_parts[idx]; actual != expected {
 			t.Errorf("%d: expected %+v, actual: %+v\n", idx, expected, actual)
 		}
 	}
@@ -66,20 +69,19 @@ func TestFindPartNumbers(t *testing.T) {
 	data, _ := utils.String_slice_file(TEST_INPUT_FILE)
 	actual_parts := find_part_numbers(data)
 
-	test_data, err := os.ReadFile("test_files/test_parts")
-	if err != nil {
+	var test_data []byte
+	var err error
+	if test_data, err = os.ReadFile("test_files/test_parts"); err != nil {
 		t.Fatalf("Error: %s\n", err)
 	}
 
 	var expected_parts []part_number
-	err = json.Unmarshal(test_data, &expected_parts)
-	if err != nil {
+	if err = json.Unmarshal(test_data, &expected_parts); err != nil {
 		t.Fatalf("Error: %s\n", err)
 	}
 
 	for idx, actual := range actual_parts {
-		expected := expected_parts[idx]
-		if actual != expected {
+		if expected := expected_parts[idx]; actual != expected {
 			t.Errorf("%d: expected: %+v, actual: %+v\n", idx, expected, actual)
 		}
 	}
@@ -89,19 +91,19 @@ func TestFindAdjacentDigits(t *testing.T) {
 	data, _ := utils.String_slice_file(TEST_INPUT_FILE)
 	actual_adjacent_digits := find_adjacent_digits(data, find_symbols(data, SYMBOLS))
 
-	test_data, err := os.ReadFile("test_files/test_adjacent_digits")
-	if err != nil {
+	var test_data []byte
+	var err error
+	if test_data, err = os.ReadFile("test_files/test_adjacent_digits"); err != nil {
 		t.Fatalf("Error: %s\n", err)
 	}
+
 	var expected_adjacent_digits []digit_coordinates
-	err = json.Unmarshal(test_data, &expected_adjacent_digits)
-	if err != nil {
+	if err = json.Unmarshal(test_data, &expected_adjacent_digits); err != nil {
 		t.Fatalf("Error: %s\n", err)
 	}
 
 	for idx, actual := range actual_adjacent_digits {
-		expected := expected_adjacent_digits[idx]
-		if expected != actual {
+		if expected := expected_adjacent_digits[idx]; expected != actual {
 			t.Errorf("%d: expected: %+v, actual: %+v\n", idx, expected, actual)
 		}
 	}
@@ -111,19 +113,20 @@ func TestFindSymbols(t *testing.T) {
 	data, _ := utils.String_slice_file(TEST_INPUT_FILE)
 	actual_symbols := find_symbols(data, SYMBOLS)
 
-	test_symbols_data, err := os.ReadFile("test_files/test_symbols")
-	if err != nil {
+	var test_symbols_data []byte
+	var err error
+	
+	if test_symbols_data, err = os.ReadFile("test_files/test_symbols"); err != nil {
 		panic(err)
 	}
 	var expected_symbols []symbol_coordinates
-	err = json.Unmarshal(test_symbols_data, &expected_symbols)
-	if err != nil {
+	
+	if err = json.Unmarshal(test_symbols_data, &expected_symbols); err != nil {
 		panic(err)
 	}
 
 	for idx, actual := range actual_symbols {
-		expected := expected_symbols[idx]
-		if expected != actual {
+		if expected := expected_symbols[idx]; expected != actual {
 			t.Errorf("%d: expected: %+v, actual: %+v\n", idx, expected, actual)
 		}
 	}
@@ -132,9 +135,8 @@ func TestFindSymbols(t *testing.T) {
 func TestSumPartNumbers(t *testing.T) {
 	data, _ := utils.String_slice_file(TEST_INPUT_FILE)
 	expected := 4361
-	actual := sum_part_numbers((data))
-	if expected != actual {
+
+	if actual := sum_part_numbers((data)); expected != actual {
 		t.Errorf("expected: %d, actual: %d\n", expected, actual)
 	}
-	t.Skip("Not yet implemented")
 }
