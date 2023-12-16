04f4a849c58f8de65eb7b3409e5d6032297331ba
Paul Koos <paully_k5298@yahoo.com>
2023-12-15 21:29:20 -0800

    add input and skeleton of the code for day four.

--- four/four.go
+++ four/four.go
@@ -1,1 +1,31 @@
 package four
+
+import (
+	"fmt"
+)
+
+const INPUT_FILE = "four/input"
+
+type Card struct {
+	ID int
+	Winning []int
+	Current []int
+	Matches int
+}
+
+func (card Card) CalculatePoints() (points int) {
+	if card.Matches == 0 {
+		return 0
+	}
+	points += 1
+	for i := 1; i<card.Matches; i++ {
+		points *= 2
+	}
+	return points
+}
+
+type Cards []Card
+
+func Run() {
+	fmt.Println("==== Day 4 ====")
+}
