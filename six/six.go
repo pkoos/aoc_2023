e3f3dec3d012c6eeeb8265e36a65e34264b50689
Paul Koos <paully_k5298@yahoo.com>
2023-12-18 22:42:20 -0800

    add functionality for day six part one.

--- six/six.go
+++ six/six.go
@@ -3,6 +3,9 @@ package six
 import (
 	"aoc_2023/utils"
 	"fmt"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 type Race struct {
@@ -11,19 +14,80 @@ type Race struct {
 }
 
 func (r Race) Possibilities() (result int) {
+	var distance int
+	for held := 0; held < r.Time; held++ {
+		if distance = held * (r.Time - held); distance > r.Distance {
+			result++
+		}
+	}
 	return result
+}
+
+func (r Race) Equals(r2 Race) (isEqual bool) {
+	isEqual = true
+	isEqual = isEqual && r.Time == r2.Time
+	isEqual = isEqual && r.Distance == r2.Distance
+
+	return isEqual
 }
 
 type Races []Race
 
+func parse_line(line string) (results []int) {
+	var val_str string
+	var found bool
+	for idx, char := range line {
+		_ = idx
+		if unicode.IsDigit(char) {
+			found = true
+			val_str += string(char)
+			if idx + 1 == len(line) {
+				value, _ := strconv.Atoi(val_str)
+				results = append(results, value)
+			}
+		} else {
+			if found {
+				value, _ := strconv.Atoi(val_str)
+				results = append(results, value)
+				found = false
+				val_str = ""
+			}
+		}
+	}
+	return results
+}
+
 func parse_races(data []string) (races Races) {
+	var times []int
+	var distances []int
+	for _, line := range data {
+		if strings.Contains(line, "Time:") {
+			times = parse_line(line)
+		}
+		if strings.Contains(line, "Distance:") {
+			distances = parse_line(line)
+		}
+	}
+
+	if len(times) != len(distances) {
+		fmt.Println("There's something wrong, these should be the same!")
+	}
+
+	for i := range times {
+		races = append(races, Race{times[i], distances[i]})
+	}
+
 	return races
 }
 
 func product_winning_possibilities(data []string) (result int) {
 	races := parse_races(data)
 	for _, race := range races {
-		result += race.Possibilities()
+		if result == 0 {
+			result += race.Possibilities()
+		} else {
+			result *= race.Possibilities()
+		}
 	}
 	return result
 }
--- six/six_test.go
+++ six/six_test.go
@@ -1,17 +1,61 @@
 package six
 
 import (
+	"aoc_2023/utils"
+	"reflect"
 	"testing"
 )
 
+const TEST_INPUT_FILE = "test_files/test_input"
+
+func TestParseLine(t *testing.T) {
+	data, _ := utils.String_slice_file(TEST_INPUT_FILE)
+	expecteds := [][]int{
+		{7, 15, 30},
+		{9, 40, 200},
+	}
+	for idx, line := range data {
+		actual := parse_line(line)
+		expected := expecteds[idx]
+		if !reflect.DeepEqual(actual, expected) {
+			t.Errorf("%d: expected: %+v, actual: %+v\n", idx, expected, actual)
+		}
+	}
+}
+
 func TestRacePossibilities(t *testing.T) {
-	t.Skip("Not yet implemented.")
+	data, _ := utils.String_slice_file(TEST_INPUT_FILE)
+	races := parse_races(data)
+	expecteds := []int {4, 8, 9}
+	for idx, race := range races {
+		expected := expecteds[idx]
+		actual := race.Possibilities()
+		if expected != actual {
+			t.Errorf("%d: expected: %+v, actual: %+v\n", idx, expected, actual)
+		}
+	}
 }
 
 func TestParseRaces(t *testing.T) {
-	t.Skip("Not yet implemented.")
+	data, _ := utils.String_slice_file(TEST_INPUT_FILE)
+	races := parse_races(data)
+	expecteds := Races{
+		{7, 9}, {15, 40}, {30, 200},
+	}
+	for idx, actual := range races {
+		expected := expecteds[idx]
+		if !actual.Equals(expected) {
+			t.Errorf("%d: expected: %+v, actual: %+v\n", idx, expected, actual)
+		}
+	}
 }
 
 func TestProductWinningPossibilities(t *testing.T) {
-	t.Skip("Not yet implemented.")
+	data, _ := utils.String_slice_file(TEST_INPUT_FILE)
+	expected := 4 * 8 * 9 // 288, r1 * r2 * r3
+	actual := product_winning_possibilities(data)
+	// t.Skip("Not yet implemented")
+	if actual != expected {
+		t.Errorf("expected: %d, actual: %d\n", expected, actual)
+	}
 }
