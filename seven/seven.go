9c27a619edad271aab1dd7bade0bda06a1d4481d
Paul Koos <paully_k5298@yahoo.com>
2023-12-18 23:50:59 -0800

    add a lot of functions to complete day seven part one, and unit tests.

--- seven/seven.go
+++ seven/seven.go
@@ -2,12 +2,75 @@ package seven
 
 import (
 	"aoc_2023/utils"
+	"cmp"
 	"fmt"
+	"slices"
 )
 
 const INPUT_FILE = "seven/input"
 
+type Hand struct {
+	Cards string
+	Bid int
+	Rank int
+	Type HandType
+}
+
+type HandType struct {
+	Name string
+	Rank int
+}
+
+type HandTypes []HandType
+
+var types HandTypes = HandTypes{
+	{"High card", 1},
+	{"One pair", 2},
+	{"Two pair", 3},
+	{"Three of a kind", 4},
+	{"Full House", 5},
+	{"Four pair", 6},
+	{"Five of a kind", 7},
+}
+
+
+type Hands []Hand
+
+func (h Hands) SortHands() { // this needs to be tested
+	slices.SortFunc[Hands](h, func(a, b Hand) (n int) {
+		if n = cmp.Compare(a.Type.Rank, b.Type.Rank); n != 0 {
+			return n
+		}
+		return cmp.Compare(a.Cards, b.Cards)
+
+	})
+}
+
+func (h Hands) Rank() {
+	// stuff and things
+}
+
+func (h Hands) CalculateWinnings() (result int) {
+	return result
+}
+
+func parse_hand(line string) (result Hand) {
+	return result
+}
+
+func parse_hands(data []string) (result Hands) {
+	for _, line := range data {
+		result = append(result, parse_hand(line))
+	}
+	return result
+}
+
 func total_winnings(data []string) (result int) {
+	hands := parse_hands(data)
+	hands.SortHands() // if rank worked correctly, then all hands should be in order
+	hands.Rank()
+	result = hands.CalculateWinnings()
+
 	return result
 }
 
--- seven/seven_test.go
+++ seven/seven_test.go
@@ -1,10 +1,58 @@
 package seven
 
 import (
+	"aoc_2023/utils"
+	"reflect"
 	"testing"
 )
 
+
+
 const TEST_INPUT_FILE = "test_files/test_input"
-func TestStuff(t *testing.T) {
-	t.Skip("Not yet implemented.")
+
+func TestParsehand(t *testing.T) {
+	data, _ := utils.String_slice_file(TEST_INPUT_FILE)
+	expecteds := Hands{
+		{"32T3K", 765, 0, HandType{"One pair", 2}},
+		{"T55J5", 684, 0, HandType{"Three of a kind", 4}},
+		{"KK677", 28,  0, HandType{"Two pair", 3}},
+		{"KTJJT", 220, 0, HandType{"Two pair", 3}},
+		{"QQQJA", 483, 0, HandType{"Three of a kind", 4}},
+	}
+	for idx, line := range data {
+		actual := parse_hand(line)
+		expected := expecteds[idx]
+		if !reflect.DeepEqual(actual, expected) {
+			t.Errorf("%d: expected: %+v, actual: %+v\n", idx, expected, actual)
+		}
+	}
+}
+
+func TestParseHands(t *testing.T) {
+	data, _ := utils.String_slice_file(TEST_INPUT_FILE)
+	hands := parse_hands(data)
+	expecteds := Hands{
+		{"32T3K", 765, 0, HandType{"One pair", 2}},
+		{"T55J5", 684, 0, HandType{"Three of a kind", 4}},
+		{"KK677", 28,  0, HandType{"Two pair", 3}},
+		{"KTJJT", 220, 0, HandType{"Two pair", 3}},
+		{"QQQJA", 483, 0, HandType{"Three of a kind", 4}},
+	}
+	for idx, actual := range hands {
+		expected := expecteds[idx]
+		if !reflect.DeepEqual(expected, actual) {
+			t.Errorf("%d: expected: %+v, actual: %+v\n", idx, expected, actual)
+		}
+	}
+}
+
+func TestTotalWinnings(t *testing.T) {
+	data, _ := utils.String_slice_file(TEST_INPUT_FILE)
+	actual := total_winnings(data)
+	expected := 765 * 1 + 220 * 2 + 28 * 3 + 684 * 4 + 483 * 5 // 6440
+	t.Skip("Not yet implemented")
+	if expected != actual {
+		t.Errorf("expected: %d, actual: %d\n", expected, actual)
+	}
+	
 }
